f21d8409d923d88939aba183e4e65c28f3658cd3
noltest106 <noltest106@gmail.com>
2021-12-13 21:56:21 +0900

    second commit

--- yahooTrain.py
+++ yahooTrain.py
@@ -1,1 +1,7 @@
-print("hello")
+from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
+
+
+driver = webdriver.Chrome(executable_path="\\wsl.localhost\Ubuntu-20.04\home\nol\automation-test2\chromedriver.exe")
+driver.get("https://yahoo.co.jp")
