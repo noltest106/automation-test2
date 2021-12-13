b96b4527c62eccf9f28a13fe39f6f277e4ed43c1
noltest106 <noltest106@gmail.com>
2021-12-13 22:29:24 +0900

    commit

--- yahooTrain.py
+++ yahooTrain.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver import Chrome
+#from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 
-driver = webdriver.Chrome(executable_path="\\wsl.localhost\Ubuntu-20.04\home\nol\automation-test2\chromedriver.exe")
+driver = webdriver.Chrome(executable_path="C:\\Users\\toshi\\AppData\\Local\\Programs\\Python\\Python39\\Lib\\site-packages\\chromedriver_binary\\chromedriver.exe")
 driver.get("https://yahoo.co.jp")
